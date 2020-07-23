b11b354d5773311ace0045bd1a48679b1f308b9b
Thiago Carvalho <tvsctj@gmail.com>
2020-07-23 14:49:26 -0300

    Terceiro commit - ALL Files

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const requireDir = require ('require-dir');
+const requireDir = require('require-dir');
 const cors = require('cors');
+
+
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect('mongodb://localhost:27017/nodeapi',{ useUnifiedTopology: true });
-requireDir ('./src/models');
+
+
+const connectionString = `mongodb+srv://thiagotot:123@cluster0.oaif9.mongodb.net/nodeapi?retryWrites=true&w=majority`
+
+try {
+    mongoose.connect(connectionString, { useNewUrlParser: true}, () =>
+        console.log("connected"));
+} catch (error) {
+    console.log("could not connect");
+}
+
+requireDir('./src/models');
 
 app.use('/api', require("./src/routes"));
 
--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -4,7 +4,7 @@ const Product = mongoose.model('Product');
 module.exports = {
     async index(req, res) {
         const {page =1}= req.query;
-        const products = await Product.paginate({},{page, limit:10});
+        const products = await Product.paginate({},{page, limit:4});
 
         return res.json(products);
     },
